d637f7923ece0462242c5a42bac92c6ec99ed7f3
Shane <docmilo@users.noreply.github.com>
2023-03-19 15:16:16 +0000

    add starter code for week 9

---
+++ Week 9/TDDBanking/TDDBanking/Model/BankAccount.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingTDD.Models;
+
+public class BankAccount
+{
+    // Private fields 
+    private List<Transaction> _allTransactions = new List<Transaction>();
+        
+
+    // Properties
+    public string AccountNumber { get; }
+    public string Owner { get; set; }
+        
+    /// <summary>
+    /// Gets the balance of the bank account.
+    /// </summary>
+    public decimal Balance
+    {
+        // TODO - implement the get. It should iterate through the all of the transactions 
+        // and apply withdrawals or deposits to the account and return the balance
+        get;
+        
+    }
+
+
+    // Constructor
+    public BankAccount(string accountNumber, string owner, decimal initialBalance)
+    {
+        // TODO - implement the constructor
+        throw new NotImplementedException();
+
+    }
+
+    /// <summary>
+    /// The MakeDeposit adds a new Transaction to the account with the given amount and note
+    /// </summary>
+    /// <param name="amount"> A amount to add to the account</param>
+    /// <param name="date">The date and time of the transaction</param>
+    /// <param name="note">Any notes associated with the transaction</param>
+    public void MakeDeposit(decimal amount, DateTime date, string note)
+    {
+        // Todo - implement the MakeDeposit method
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// The MakeWithdrawal adds a new Transaction to the account with the given amount and note. We 
+    /// should check that the withdrawal amount does not exceed the account balance. If it does throw
+    /// an argument exception. 
+    /// </summary>
+    /// <param name="amount">The amount to withdraw from the account</param>
+    /// <param name="date"></param>
+    /// <param name="note"></param>
+    /// <exception cref="ArgumentException"></exception>
+    public void MakeWithdrawal(decimal amount, DateTime date, string note)
+    {
+        // TODO - implement the MakeWithdrawal method
+        throw new NotImplementedException();
+    }
+}
+
---
+++ Week 9/TDDBanking/TDDBanking/Model/Transaction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingTDD.Models;
+public enum TransactionType
+{
+    Deposit,
+    Withdrawal,
+    Transfer
+}
+/// <summary>
+/// The Transaction class represents a transaction of type <see cref="TransactionType"/> on a Bank
+/// account. Transactions can be applied to an <see cref="BankAccount"/>account to determine the 
+/// current balance of the account. Each BankAccount contains a list of transactions. 
+/// </summary>
+public class Transaction
+{
+    // Properties
+    public string AccountNumber { get; }
+    public decimal Amount { get; }
+    public DateTime Date { get; }
+    public TransactionType TransType { get; }
+    public string Notes { get; }
+
+
+    /// <summary>
+    /// Transaction objects represent transactions that occur on an account such as withrdrawals, deposits, and transfers.
+    /// </summary>
+    /// <param name="amount">The transaction amount.</param>
+    /// <param name="date">The date and time of the transaction.</param>
+    /// <param name="transactionType">The type of transaction made.</param>
+    /// <param name="note">Additional notes on the transaction</param>
+    public Transaction(string accountNumber, decimal amount, DateTime date, TransactionType transactionType, string note)
+    {
+        AccountNumber = accountNumber;
+        Amount = amount;
+        Date = date;
+        TransType = transactionType;
+        Notes = note;
+    }
+}
+
