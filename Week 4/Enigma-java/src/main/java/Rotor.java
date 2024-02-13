e0442f18d594853d807e93b8bba529d834567c9e
James McGinley <158051422+JMG-Student@users.noreply.github.com>
2024-02-13 12:47:45 +0000

    Added finished rotorEncrypt method to Rotor.java

--- Week 4/Enigma-java/src/main/java/Rotor.java
+++ Week 4/Enigma-java/src/main/java/Rotor.java
@@ -1,12 +1,44 @@
+import java.util.List;
+
 public class Rotor 
 {
 
-    public String rotorEncrypt(String msg, String rotor)
+    public String rotorEncrypt(String msg, List<String> rotors)
     {
-        return "Add Method when reviewed";
+        final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String encryptedMsg = "";
+        for (int i = 0; i < rotors.size(); i++)
+        {
+            //Swaps/resets variables after the first loop
+            if(i > 0)
+            {
+                msg = encryptedMsg;
+                encryptedMsg = "";
+            }
+
+            for(char letter: msg.toCharArray())
+            {
+                if(alphabet.indexOf(letter) == -1)
+                {
+
+                    encryptedMsg += letter;
+
+                }
+                else{
+
+                    //Gets the index of where the msg character is on the alphabet and adds its rotor counterpart to encryptedMsg
+                    encryptedMsg += rotors.get(i).charAt(alphabet.indexOf(letter));
+
+                }
+                
+            }
+
+        }
+        
+        return encryptedMsg;
     }
 
-    public String rotorDecrypt(String msg, String rotor)
+    public String rotorDecrypt(String msg, List<String> rotors)
     {
         return "Add Method when reviewed";
     }
