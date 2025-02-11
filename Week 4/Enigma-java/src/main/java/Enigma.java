96561158e507d97e64a7bf422e493e617d7e271b
Ethan Curran - STUDENT <l00181476@atu.ie>
2025-02-11 17:03:35 +0000

    StartMessage

--- Week 4/Enigma-java/src/main/java/Enigma.java
+++ Week 4/Enigma-java/src/main/java/Enigma.java
@@ -52,16 +52,24 @@ public final class Enigma {
         // 2. Apply the CAESAR shift
         // 3. Return the decrypted string
 
-        String DecryptedMessage = "";
+        String DencytpedMessage = "";
         message = message.toUpperCase();
         String newMessage  = "";
         int index = 0;
-        String regAl = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        for(int x = 0;x<rotors.size();x--)
+        String regAl = rotors.get(rotors.size()-1);
+        String rotor = "";
+        for(int x = 0;x<rotors.size();x++)
         {
-            String rotor = rotors.get(x);
+            if(x!=2)
+            {
+                rotor = rotors.get(rotors.size()-(2+x));
+            }
+            else
+            {
+                rotor = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
             newMessage = "";
-            for(int y = 0;y<message.length();y--)
+            for(int y = 0;y<message.length();y++)
             {
                 
                 index = regAl.indexOf(message.substring(y,y+1));
@@ -74,13 +82,34 @@ public final class Enigma {
                 }
                 
             }
-            DecryptedMessage = newMessage;
+            System.out.println(regAl);
+            if(x!=2)
+            {
+            regAl = rotors.get(rotors.size()-(2+x));
+            }
+            DencytpedMessage = newMessage;
+            message = newMessage;
+            System.out.println(rotor);
+
+            System.out.println(DencytpedMessage);
         }
+        // Steps in brief
+        // 1. For each rotor in the list rotors, starting with the last rotor
+        //  1.1 Translate the message using the rotor
+        // 2. Apply the CAESAR shift
+        // 3. Return the decrypted string
+
+
+        return DencytpedMessage;
+
 
 
         // This is going to be amazing
 
-        return DecryptedMessage;
+
 
     }
 }
+
+    
+
