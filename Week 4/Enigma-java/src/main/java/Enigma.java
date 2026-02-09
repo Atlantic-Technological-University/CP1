b2b7649d18f8bc81232fdbe62f5288d1b9c4dbc2
Katelyn M <255952759+katem26@users.noreply.github.com>
2026-02-09 13:30:58 +0000

    Started rotor decryption

--- Week 4/Enigma-java/src/main/java/Enigma.java
+++ Week 4/Enigma-java/src/main/java/Enigma.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public final class Enigma {
 
+    static String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static String Encrypt(String message, int incrementNumber,List<String> rotors ){
         // TODO - Implement the Encrypt method
         // Steps in brief
@@ -21,11 +23,19 @@ public final class Enigma {
     public static String Decrypt(String message, int incrementNumber, List<String> rotors)
     {
         // TODO - Implement the Decrypt method
-
         // for each rotor, starting with the last one
+        StringBuilder m = new StringBuilder(message.toUpperCase());
+
         for (int i=rotors.size()-1; i>0; i--)
         {
-
+            for (int j=0; j<m.length(); j++)
+            {
+                if (alphabet.indexOf(m.charAt(j)) != -1) // if the character is in the alphabet
+                {
+                    char translatedCharacter = alphabet.charAt(...);
+                    m.setCharAt(j, '?');
+                }
+            }
         }
 
         // John stuff
