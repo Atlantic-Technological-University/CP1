968043ac32bb838d32ef1533ccc040d2bb11c94e
DyaDyaBogdan737 <257294789+DyadyaBogdan737@users.noreply.github.com>
2026-02-14 15:41:14 +0000

    Added Rotor shift method just in case

--- Week 4/Enigma-java/src/main/java/Enigma.java
+++ Week 4/Enigma-java/src/main/java/Enigma.java
@@ -10,13 +10,11 @@ public final class Enigma {
 
     public static String CAESARshift(String message, int incrementNumber){
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        // char CAESARShift [] = new char[26];
+        // char decodedWord [] = {};
 
-        char CAESARShift [] = new char[26];
-
-        char decodedWord [] = {};
-        decodedWord = message.toUpperCase().toCharArray();
-
-        CAESARShift = alphabet.toCharArray();
+        char [] decodedWord = message.toUpperCase().toCharArray();
+        char [] CAESARShift = alphabet.toCharArray();
 
         for (int i = 0; i < decodedWord.length; i++) {
             int CAESERShiftInLetters = 0;
@@ -31,13 +29,11 @@ public final class Enigma {
                 // If character is NOT spaceBar, then do CAESARShift
                 if (decodedWord[i] == CAESARShift[j]) {
                     CAESERShiftInLetters = j+incrementNumber+i;
-                    System.out.println(j);
+                    // System.out.println(j);
                     break;
                 }
             }
             
-
-
 
             if(CAESERShiftInLetters > 26){
                 CAESERShiftInLetters -= 26;
@@ -49,7 +45,6 @@ public final class Enigma {
         }
 
         String encryptedWord = "";
-
         for (char c : decodedWord) {
             encryptedWord += c;
         }
@@ -57,57 +52,75 @@ public final class Enigma {
         return encryptedWord;
     }
 
+
+    public static String RotorsShift(String message, int incrementNumber, List<String> rotors){
+        // For test purpose array below
+        // char [] CAESARAppliedWord = {'H', 'E', 'L', 'L', 'O', ' ', 'W', 'O', 'R', 'L', 'D'};
+        char [] CAESARAppliedWord = message.toUpperCase().toCharArray();
+        
+        // Actual rotor array used to encode
+        ArrayList<Character> rotor = new ArrayList<Character>();
+
+        // Alphabet array
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char [] alphabetArray = alphabet.toCharArray();
+        ArrayList<Character> alphabetCharacters = new ArrayList<Character>();
+
+        for (Character c : alphabetArray) {
+            alphabetCharacters.add(c);
+        }
+
+
+        for(int i = 0; i < rotors.size(); i++){
+            // Buffer array to extract characters
+            char [] rotorLetters = rotors.get(i).toCharArray();
+
+            // Fill the rotor array
+            for(int z = 0; z < rotorLetters.length; z++){
+                rotor.add(z, rotorLetters[z]);
+            }
+
+            // Apply rotors encoding
+            for(int j = 0; j < CAESARAppliedWord.length; j++){
+                // System.out.println(alphabetCharacters);
+                if(CAESARAppliedWord[j] == ' '){
+                    continue;
+                }
+                else {
+                    int shift = alphabetCharacters.indexOf(CAESARAppliedWord[j]);
+                    // System.out.print(rotor.get(shift) + ", ");
+                    CAESARAppliedWord[j] = rotor.get(shift);
+                }
+            }
+
+            // Clear previous rotor after being used
+            rotor.clear();
+        }
+
+        // Assemble encoded word
+        String encodedWord = "";
+        for (char  c : CAESARAppliedWord) {
+            encodedWord += c;
+        }
+        
+
+        return encodedWord;
+    }
+
+
     public static String Encrypt(String message, int incrementNumber,List<String> rotors ){
         // TODO - Implement the Encrypt method
         // Steps in brief
+
         // 1. Apply the CAESAR shift using the increment number
-        CAESARshift(message, incrementNumber);
-
-
-        // Part 2 rotors shift
-
-
-
-        // for (int i = 0; i < rotors.size(); i++) {
-        //     for (int j = 0; j < decodedWord.length; j++) {
-        //         // encryptedCode = rotors.get(i).toCharArray();
-        //         // ArrayList<Character> encryptedCode = new ArrayList<Character>();
-        //         ArrayList<Character> sample = new ArrayList<Character>();
-                
-        //         for(char c : rotors.get(i).toCharArray()){
-        //             sample.add(c);
-        //         }
-
-        //         char current = decodedWord[j];
-
-        //         System.out.print(sample.indexOf(current) + " ," + sample.size());
-
-        //         int shiftInLetters = j+incrementNumber;
-
-        //         if(shiftInLetters > 26){
-        //             shiftInLetters -= 26;
-        //         }
-
-        //         decodedWord[j] = encryptedCode[shiftInLetters];
-
-        //         sample.clear();
-        //         System.out.println(sample);
-        //     }            
-        // }
-
-        // String encryptedWord = "";
-
-        // for (char c : decodedWord) {
-        //     encryptedWord += c;
-        // }
-
-        // return encryptedWord;
+        // Method CAESARshift was created
 
         // 2. For each rotor in the list rotors
         //  2.1 Translate the message using the rotor
         // 3. Return the encrypted string
+        // Method RotorsShift was created - in case it was not done
 
-        return "Implement the encrypt method";
+        return RotorsShift(CAESARshift(message, incrementNumber), incrementNumber, rotors);
     }
 
 
--- Week 4/Enigma-java/src/main/java/Main.java
+++ Week 4/Enigma-java/src/main/java/Main.java
@@ -19,20 +19,14 @@ public class Main {
         // ToDO
         // Get a start message, encrypt it and then decrypt it
 
-        String startMessage = "Hello world";
+        // String startMessage = "ABCD";
+        String startMessage = "Hello World";
 
-        // char b = startMessage.charAt(0);
 
-        // char letters [] = new char [120];
-
-        // letters = startMessage.toCharArray();
-
-        // for (char c : letters) {
-        //     System.out.println(c);
-        // }
-
-        // System.out.println(b);
+        
         String encryptedMessage = Enigma.Encrypt(startMessage, 4, rotors);
+        // String encryptedMessage = Enigma.RotorsShift(startMessage, 4, rotors);
+
         System.out.println("The encrypted message is: " + encryptedMessage);
 
         String decryptedMessage = Enigma.Decrypt(encryptedMessage, 4, rotors);
